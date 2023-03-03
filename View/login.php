c21de954b75a6fefddf4dde3dcad651eb99d5102
Djordje Stefanovic <stefanovic.djordje1997@gmail.com>
2023-03-03 18:15:51 +0100

    Napravljena login stranica

--- View/login.php
+++ View/login.php
@@ -6,10 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-
+    <div class="login-box">
+        <h2>Login</h2>
+        <form method="POST" action="#">
+            <div class="user-box">
+                <label for="">Username</label>
+                <input type="text" name="username">
+            </div>
+            <div class="user-box">
+                <label for="">Password</label>
+                <input type="password" name="password">
+                <button type="submit" class="btn btn-primary" name="submit">Uloguj se</button>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
